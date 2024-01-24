0f665c6d3d0764856f6ef8d085b233a1e9346cc2
Matthew Barker <matthew.j.barker1@gmail.com>
2024-01-24 14:01:59 +0000

    fix build error

---
+++ client.ts
@@ -0,0 +1,7 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+  projectId: 'sgx0hyut', // you can find this in sanity.json
+  dataset: 'production', // or the name you chose in step 1
+  useCdn: true // `false` if you want to ensure fresh data
+})
